3bf8d4f46788868e9cf2b667bb4ac2c5b46ea989
pepynd1r-work <74298965+pepynd1r-work@users.noreply.github.com>
2022-10-14 12:56:45 +0300

    remove

--- spec/pepynd1r-package-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Pepynd1rPackageView from '../lib/pepynd1r-package-view';
-
-describe('Pepynd1rPackageView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
